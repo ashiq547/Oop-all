8ef9570d1b5c6026c52f80e8da26c4a061415364
Abrar Faiaz <2221445@iub.edu.bd>
2024-12-08 08:44:37 -0800

    CitationScene

--- group-19-sec-2-oop/PublicLibrary/src/main/java/module-info.java
+++ group-19-sec-2-oop/PublicLibrary/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.example.publiclibrary {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens org.example.publiclibrary to javafx.fxml;
--- group-19-sec-2-oop/PublicLibrary/src/main/java/org/example/publiclibrary/AbrarCitationScene.java
+++ group-19-sec-2-oop/PublicLibrary/src/main/java/org/example/publiclibrary/AbrarCitationScene.java
@@ -1,5 +1,10 @@
 package org.example.publiclibrary;
 
+import javafx.scene.control.ComboBox;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
 public class AbrarCitationScene
 {
     @javafx.fxml.FXML
