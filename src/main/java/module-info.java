20e1bd667691432c9a15e00079494eb2107320d1
Abrar Faiaz <2221445@iub.edu.bd>
2024-12-10 09:01:38 -0800

    update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.sajid2222122 {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    requires java.logging;
 
 
     opens com.example.sajid2222122 to javafx.fxml;
