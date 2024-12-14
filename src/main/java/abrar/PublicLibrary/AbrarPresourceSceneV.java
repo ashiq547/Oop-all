207bb1fe7d60732345427519032ac5487be1431e
Abrar Faiaz <2221445@iub.edu.bd>
2024-12-13 21:34:21 -0800

    abrarPresourceSceneV.fxml

--- src/main/java/abrar/PublicLibrary/AbrarPresourceSceneV.java
+++ src/main/java/abrar/PublicLibrary/AbrarPresourceSceneV.java
@@ -1,7 +1,19 @@
-package org.example.publiclibrary;
+package abrar.PublicLibrary;
+
+import javafx.event.ActionEvent;
 
 public class AbrarPresourceSceneV
 {
     @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    public void handleDownloadResource(ActionEvent actionEvent) {
+    }
+
+    public void handleBackToMenus(ActionEvent actionEvent) {
+    }
+
+    public void handleFetchResources(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/org/example/publiclibrary/AbrarPresourceSceneV.java
@@ -0,0 +1,29 @@
+package org.example.publiclibrary;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+
+public class AbrarPresourceSceneV
+{
+
+    @javafx.fxml.FXML
+    private TextArea textAreaResource;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void handleBackToMenus(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleDownloadResource(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleFetchResources(ActionEvent actionEvent) {
+    }
+
+
+}
